0aa58e7d991858afa9575e78046d1416d2866907
Kevyn Posadas <tote.kev@gmail.com>
2025-12-01 10:54:29 -0600

    se añade middleware

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/routes";
+import { AuthMiddleware } from "./middlewares/auth.middleware";
 import { NewSongRoutes } from "./new-song/routes";
 
 export class AppRoutes {
@@ -7,8 +8,12 @@ export class AppRoutes {
     static get routes(): Router {
 
         const router = Router();
-        router.use('/api', NewSongRoutes.routes);
         router.use('/api/auth', AuthRoutes.routes);
+
+
+        router.use([AuthMiddleware.validateJWT]);
+        router.use('/api', NewSongRoutes.routes);
+
         return router;
 
     };
